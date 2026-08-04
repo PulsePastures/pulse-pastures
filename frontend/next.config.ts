49122ed95480d446892cd360f191c678a5338611
PulsePastures Team <team@pulsepastures.com>
2026-08-04 09:24:58 +0300

    Switch to static HTML export for ultra fast static CDN Vercel deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
   outputFileTracingRoot: path.resolve(__dirname),
 };
